227a7f6c91cfe18be17723bb32e25f49c762d1d8
Deanna Thomas <36423542+thoma23d@users.noreply.github.com>
2021-01-30 23:56:42 -0500

    Add files via upload

---
+++ static/js/portfolio.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {stickyHeader()};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
